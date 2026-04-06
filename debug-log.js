1b36f339653ebece97b2ffb69542505f90eb57a8
msw2004727 <msw741121@gmail.com>
2026-04-06 17:26:33 +0800

    debug: 檢查 opLog 和 reg 的時間格式

--- debug-log.js
+++ debug-log.js
@@ -1,20 +1,38 @@
 (function() {
-  var body = document.getElementById('event-reg-log-body');
-  if (!body) { alert('not found'); return; }
-  var items = body.children;
-  var msg = '';
-  for (var i = 0; i < items.length; i++) {
-    var cls = items[i].querySelector('.event-reg-log-action');
-    var type = cls ? cls.textContent.trim() : '?';
-    if (type !== '報名' && type !== '取消') {
-      var msEl = items[i].getAttribute('data-ms');
-      msg += '[' + i + '] ms=' + msEl + ' ' + type + '\n';
+  var logs = ApiService.getOperationLogs();
+  var pLogs = [];
+  for (var i = 0; i < logs.length; i++) {
+    if (logs[i].type === 'auto_promote' || logs[i].type === 'force_promote') {
+      pLogs.push(logs[i]);
     }
   }
-  if (!msg) msg = 'no promote/demote found';
-  msg += '\n--- first 3 ---\n';
-  for (var j = 0; j < 3 && j < items.length; j++) {
-    msg += '[' + j + '] ms=' + items[j].getAttribute('data-ms') + '\n';
+  var msg = 'opLogs loaded: ' + logs.length + '\npromote: ' + pLogs.length + '\n\n';
+
+  if (pLogs.length > 0) {
+    var l = pLogs[0];
+    msg += '_docId: ' + l._docId + '\n';
+    msg += 'createdAt type: ' + typeof l.createdAt + '\n';
+    if (l.createdAt && typeof l.createdAt === 'object') {
+      msg += 'createdAt keys: ' + Object.keys(l.createdAt).join(',') + '\n';
+      msg += 'seconds: ' + l.createdAt.seconds + '\n';
+      msg += '_seconds: ' + l.createdAt._seconds + '\n';
+      msg += 'toMillis: ' + typeof l.createdAt.toMillis + '\n';
+    }
   }
+
+  // 測試 registration 時間
+  var regs = ApiService._src('registrations');
+  if (regs.length > 0) {
+    var r = regs[0];
+    msg += '\n--- reg[0] ---\n';
+    msg += 'registeredAt type: ' + typeof r.registeredAt + '\n';
+    if (r.registeredAt && typeof r.registeredAt === 'object') {
+      msg += 'keys: ' + Object.keys(r.registeredAt).join(',') + '\n';
+      msg += 'toMillis: ' + typeof r.registeredAt.toMillis + '\n';
+    } else {
+      msg += 'value: ' + String(r.registeredAt).substring(0, 30) + '\n';
+    }
+  }
+
   alert(msg);
 })();
