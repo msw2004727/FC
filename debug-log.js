335b03670316b21be951bab61eb83bafbefd764a
msw2004727 <msw741121@gmail.com>
2026-04-06 17:36:00 +0800

    debug: 模擬 _toMs 解析 opLog + reg 時間

--- debug-log.js
+++ debug-log.js
@@ -1,37 +1,48 @@
 (function() {
+  // 模擬 _toMs
+  function toMs(v) {
+    if (!v) return 0;
+    if (typeof v === 'number') return v;
+    if (typeof v.toMillis === 'function') { try { return v.toMillis(); } catch(e) {} }
+    if (typeof v.toDate === 'function') { try { return v.toDate().getTime(); } catch(e) {} }
+    if (typeof v === 'object') {
+      var s = v.seconds || v._seconds;
+      if (typeof s === 'number') return (s * 1000) + Math.floor(((v.nanoseconds || v._nanoseconds || 0) / 1000000));
+    }
+    var t = new Date(v).getTime();
+    return isFinite(t) ? t : 0;
+  }
+
+  // 測試 operation log
   var logs = ApiService.getOperationLogs();
-  var pLogs = [];
+  var sample = null;
   for (var i = 0; i < logs.length; i++) {
-    if (logs[i].type === 'auto_promote' || logs[i].type === 'force_promote') {
-      pLogs.push(logs[i]);
-    }
+    if (logs[i].type === 'auto_promote') { sample = logs[i]; break; }
   }
-  var msg = 'opLogs loaded: ' + logs.length + '\npromote: ' + pLogs.length + '\n\n';
-
-  if (pLogs.length > 0) {
-    var l = pLogs[0];
-    msg += '_docId: ' + l._docId + '\n';
-    msg += 'createdAt type: ' + typeof l.createdAt + '\n';
-    if (l.createdAt && typeof l.createdAt === 'object') {
-      msg += 'createdAt keys: ' + Object.keys(l.createdAt).join(',') + '\n';
-      msg += 'seconds: ' + l.createdAt.seconds + '\n';
-      msg += '_seconds: ' + l.createdAt._seconds + '\n';
-      msg += 'toMillis: ' + typeof l.createdAt.toMillis + '\n';
-    }
+  var msg = '';
+  if (sample) {
+    var ms1 = toMs(sample.createdAt);
+    var docMatch = String(sample._docId || '').match(/op_(\d{13,})/);
+    var ms2 = docMatch ? Number(docMatch[1]) : 0;
+    msg += 'opLog ms via createdAt: ' + ms1 + '\n';
+    msg += 'opLog ms via _docId: ' + ms2 + '\n';
+    msg += 'date: ' + new Date(ms1 || ms2) + '\n\n';
+  } else {
+    msg += 'no auto_promote found\n\n';
   }
 
-  // 測試 registration 時間
+  // 測試 registration（用 Firestore 查的那場活動）
   var regs = ApiService._src('registrations');
   if (regs.length > 0) {
-    var r = regs[0];
-    msg += '\n--- reg[0] ---\n';
-    msg += 'registeredAt type: ' + typeof r.registeredAt + '\n';
-    if (r.registeredAt && typeof r.registeredAt === 'object') {
-      msg += 'keys: ' + Object.keys(r.registeredAt).join(',') + '\n';
-      msg += 'toMillis: ' + typeof r.registeredAt.toMillis + '\n';
-    } else {
-      msg += 'value: ' + String(r.registeredAt).substring(0, 30) + '\n';
+    var ms3 = toMs(regs[0].registeredAt);
+    msg += 'reg[0] ms: ' + ms3 + '\n';
+    msg += 'date: ' + new Date(ms3) + '\n';
+    msg += 'type of registeredAt: ' + typeof regs[0].registeredAt + '\n';
+    if (typeof regs[0].registeredAt === 'string') {
+      msg += 'value: ' + regs[0].registeredAt.substring(0, 30) + '\n';
     }
+  } else {
+    msg += 'no registrations in cache\n';
   }
 
   alert(msg);
